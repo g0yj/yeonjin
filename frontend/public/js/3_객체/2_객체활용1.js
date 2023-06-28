64f38de662efa2487a57d3a3665017f9367eb7f0
504 <504@DESKTOP-2TRGUA3>
2023-06-28 16:19:54 +0900

    JS 과제2 완

---
+++ frontend/public/js/3_객체/2_객체활용1.js
@@ -0,0 +1,59 @@
+//1. 등록함수 (회원가입 버튼 클릭할 때)
+
+let 회원리스트=[]  //1-3번에서 말한 거. 모든 함수에 접근할 수 있는 배열 선언
+
+
+function 등록(){console.log('등록()실행')
+	//1-1 입력값 가져오기
+	let mid= document.querySelector('#mid').value ; console.log(mid);
+	let mpwd= document.querySelector('#mpwd').value ; console.log(mpwd);
+	arguments
+		//유효성검사1
+		if(mid==''||mpwd==''){// return으로 받을 수도 있음. 여기선 if-else로 하겠음 
+			alert('[회원가입실패]: 회원정보 모두 입력해주세요')
+		} else{//공백이 아니면 , 유효성검사2
+				if(mid.length<8||)}
+
+	//1-2 객체담기 (입력된 데이터(mid,mpwd)를 {}안에서 속성명(아이디,비밀번호)붙여 저장)
+	let member={아이디: mid, 비밀번호: mpwd}
+	console.log(member)
+	
+	//1-3 등록할때 (함수가 실행될 )마다 객체 생성되고 '}'가 끝나면 사라짐.
+	// 밖에 전역변수 만들어 저장.= 객체 여러개 (배열)
+	회원리스트.push(member) ; console.log(회원리스트)
+	
+	/*회원리스트.push(
+		{아이디:document.querySelector('#mid').value,
+		 비밀번호: document.querySelector('#mpwd').value
+		 }) 위에 세줄을 이렇게 줄일 수 있음 */
+}
+
+//2. 로그인(로그인 버튼을 클릭했을 때)
+function 로그인(){console.log('로그인()실행')
+	//2-1 입력값 가져오기
+	let mid2= document.querySelector('#mid2').value ; console.log(mid2);
+	let mpwd2= document.querySelector('#mpwd2').value ; console.log(mpwd2);
+	//2-2 입력된 값이 존재하는지 검사
+		
+		//로그인 상태 저장 변수
+		let login=false;
+		//-회원리스트(전체) 중에서 회원1명(1개씩)꺼내서 검사하는데 아이디와 비밀번호 일치 찾기
+		for(let i=0; i<회원리스트.length ; i++){
+			// i는 0부터 마지막인덱스까지 1씩 증가 반복-> 회원리스트 중에 한명씩 꺼냄
+			let member= 회원리스트[i]//i번째 인덱스의 객체1개 꺼내기
+			if(member.아이디==mid2){//꺼낸 객체의 아이디가 입력한 아이다와 같으면
+				if(member.비밀번호==mpwd2){//i 번째 객체의 비밀번호가 입력한 비밀번호와 같으면}
+					login=true; // i번째 객체는 로그인 성공했다는 '증거'를 변수에 저장. 위에 login 변수 
+					//로그인성공 1회성이기 때문에 로그인 성공시 다른 객체 볼 필요 없음! 반복문 종료됨
+					i=회원리스트.length// 방법1. 조건을 false로 만들자
+					break; // 방법2/ break;사용. 가장 가까운 반복문 강제 종료
+					
+					}
+			
+				}	
+	
+			}// if2 조건 모두 충족했을 때 login 변수는 true 값으로 변경
+			// 판단: 로그인 상태 저장 변수
+			if(login==true){alert('로그인성공')}
+			else{alert('로그인실패')}
+	}
--- frontend/public/js/과제/과제2/과제2.js
+++ frontend/public/js/과제/과제2/과제2.js
@@ -1,81 +1,60 @@
-/*- 파일명 : 과제2.html / 과제2.js / 과제2.css
--------------------------------------------------------------------
-요구사항1 : 아이디와 비밀번호를 입력받아 회원가입을 진행하고 싶어요.
-	[조건1] 
-		입력 <input> 사용 해서 아이디 입력받기 
-		입력 <input> 사용 해서 비밀번호 입력받기 
-	[조건2]
-		* 1차원배열만 사용 [ 배열 변수 여러개 사용 가능 ]
-	[조건3]
-		<input type="button"> 사용해서 회원가입 이벤트 실행 [ 회원가입함수 구현하기 ]
 
--------------------------------------------------------------------
-요구사항2 : 아이디와 비밀번호를 입력받아 로그인 을 진행하고 싶어요.
-	[조건1]
-		입력 <input> 사용 해서 아이디 입력받기 
-		입력 <input> 사용 해서 비밀번호 입력받기 
-	[조건2] 
-		만약에 배열에 존재하는 아이디,비밀번호가 일치한 데이터가 있을경우
-		[ 로그인 성공 ] 아니면  [ 로그인 실패 ] alert 로 출력해주세요.
-	[조건3]
-		<input type="button"> 사용해서 로그인 이벤트 실행 	[ 로그인함수 구현하기 ]
-------------------------------------------------------------------------
-요구사항3 : 유효성검사 진행해주세요.  
-	1. 만약에 입력상자[input] 에 공백 이면 학번을 입력해주세요~ 		[ if( sno2Value == '' ) ]
-	2. 회원가입 이나 로그인시 성공시에 입력상자[input] 의 value 다시 공백으로 초기화 해주세요.	[ sno2.value = '' ]
-	3. 입력상자[input]에 입력한 아이디 와 비밀번호 가 8자리가 이상일경우에만 회원가입하도록 해주세요.	[ sno2Value.length !=8  ]
-
-요구사항4 : 기본적인 css 작업해주세요
-	1. (필수) div 2개를 만들어서 [ 왼쪽은 회원가입 구역 ] [ 오른쪽은 로그인 구역 ]
-	2. 그외 이쁘게 꾸며주세요. */
-	
-//--------요구사항1------------------------------------
-let id리스트=[]
-let pw리스트=[]
-function 회원가입함수(){
-	let id=document.querySelector('.inputId').value
-	let pw=document.querySelector('.inputPw').value
-	console.log('id출력: '+id)
-	console.log('비밀번호: '+pw)
-	id리스트.push(id)
-	pw리스트.push(pw)
-	console.log(id리스트)
-	console.log(pw리스트)
-}
-
-
-//--------요구사항2-------------------------------------
-function 로그인함수(){
-	let id=document.querySelector('.loginId').value
-	let pw=document.querySelector('.loginPw').value
-	
-	if(id리스트==id&&pw리스트==pw){
-		alert('로그인 성공')
-		}
-	else{alert('로그인 실패')}	
-	
-	}
-	
-	
 //-----------요구사항3--------------------------------
 /*1. 만약에 입력상자[input] 에 공백 이면 학번을 입력해주세요~ 		[ if( sno2Value == '' ) ]
   2. 회원가입 이나 로그인시 성공시에 입력상자[input] 의 value 다시 공백으로 초기화 해주세요.	[ sno2.value = '' ]
   3. 입력상자[input]에 입력한 아이디 와 비밀번호 가 8자리가 이상일경우에만 회원가입하도록 해주세요.	[ sno2Value.length !=8  ]
 */
-let id리스트1=[]
-let pw리스트1=[]
-
-function 로그인함수1(){
-	let id1=document.querySelector('.loginId').value
-	let pw1=document.querySelector('.loginPw').value
-	
-	if(id1==''){alert('학번입력해주셈')}
-	
-	id1=``
-	pw1=``
-}
 
 /*
 1. 아이디,비밀번호 입력창 / 회원가입,로그인 버튼 만들기
-2. 
+2. 로그인 함수 만들기
+- queryselector로 value값 받기
+- input박스가 공백이면 학번 입력 alert띄움
+- id리스트 배열 내 있는지 확인 indexOf = -1 , pw리스트 배열 내 있는지 확인
+ 	ㄴ??????????? 같은 인덱스 끼리 묶는 방법?!?!?
+- if문으로 각 배열의 인덱스의 값이 같으면 로그인성공으로 alert창 띄움
+- 초기화 `` 해서 input의 value 제거
+
+3. 회원가입 함수 만들기
+-queryselctor value 값 받기
+-id리스트에 없는 값일 때 실행
+-아이디 길이 8이상 && 비밀번호길이 8이상일때 실행
+-초기화
 */
+
+let id리스트3=[]
+let pw리스트3=[]
+
+function 로그인함수1(){
+	let id3= document.querySelector('.loginId3').value
+	let pw3= document.querySelector('.loginPw3').value
+	console.log('로그인함수 아이디 받음: '+id3)
+	console.log('로그인함수 비밀번호 받음: '+pw3)
+	//아이디 공백이면 학번 입력
+	if(id3==''){alert('학번 입력 해주셈')
+					return;} 
+	for(let i=0; i<id리스트3.length;i++){
+		if(id리스트3[i]==id3&&pw리스트3[i]==pw3){alert('로그인성공')}
+	}
+	//초기화
+	document.querySelector('.loginId3').value=``
+	document.querySelector('.loginPw3').value=``
+}
+
+function 회원가입함수1(){
+	id3= document.querySelector('.loginId3').value
+	pw3= document.querySelector('.loginPw3').value
+	if(id리스트3.indexOf(id3)!=-1){
+		alert('가입 불가능')
+		return;
+	}
+	if(id3.length>=8||pw3.length>=8){id리스트3.push(id3)
+						pw리스트3.push(pw3);
+						alert('성공')}
+	console.log(id리스트3)					
+	console.log(pw리스트3)					
+
+	
+	document.querySelector('.loginId3').value=``
+	document.querySelector('.loginPw3').value=``
+}
