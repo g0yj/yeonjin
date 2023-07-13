ce94831c7e3c2b4f59ed302d0a48254063caf3e8
504 <504@DESKTOP-2TRGUA3>
2023-07-13 18:00:18 +0900

    과제1

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,30 @@
+package 과제.과제1;
+
+public class 과제1 {
+
+	public static void main(String[] args) {
+
+
+		/*
+		1. 4
+		2. 2,3,4,5,6,7
+		3.
+			정수타입: byte / short,char / int/ long
+			실수: float / double
+		4. 
+			타입: int, double
+			변수이름: age, price
+			리터럴: 10, 3.14
+		5. 3
+		6. 4
+		7. 6
+		8. 3
+		9.1
+		10. 1
+		11. 13,16	
+		*/
+		
+
+	}
+
+}
